e9e3cda1eca719fd00dec4609bced1676e787223
hdurand22 <hdurand22@gmail.com>
2022-02-01 10:22:29 -0800

    updated styling to remove react-bootstrap

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,30 +1,20 @@
 import React from 'react';
-// import {
-//     CDBSidebar,
-//     CDBSidebarContent,
-//     CDBSidebarFooter,
-//     CDBSidebarHeader,
-//     CDBSidebarMenu,
-//     CDBSidebarMenuItem
-// } from 'cdbreact';
-import { Image } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
-import { Link as ScrollLink, animateScroll as Scroll } from 'react-scroll';
+import { Link as ScrollLink } from 'react-scroll';
 
 const Sidebar = () => {
     const location = useLocation();
-    // console.log(`location: ${location.pathname}`);
     let isHomePage = false;
     if (location.pathname === '/') {
         isHomePage = true;
     }
-    console.log(`isHomePage: ${isHomePage}`);
+    // console.log(`isHomePage: ${isHomePage}`);
 
     return (
         <div id="header">
             <div className="top">
                 <div id="logo">
-                    <span className="image avatar48"><Image src="/images/profile.png" alt="Alexandra To" /></span>
+                    <span className="image avatar48"><img src="/images/profile.png" alt="Alexandra To" /></span>
                     <h1 id="title">Alexandra To</h1>
                     <p>HCI Researcher</p>
                 </div>
@@ -92,9 +82,9 @@ const Sidebar = () => {
             </div>
             <div className="bottom">
                 <ul className="icons">
-                    <li><a href="https://twitter.com/alexandra__to" className="icon brands fa-twitter fa-2x"><span className="label">Twitter</span></a></li>
-                    <li><a href="mailto:a.to[at]northeastern.edu" className="icon solid fa-envelope fa-2x"><span className="label">Email</span></a></li>
-                    <li><a href="https://scholar.google.com/citations?user=FB_pYfIAAAAJ&hl=en" className="icon solid fa-graduation-cap fa-2x"><span className="label">Google Scholar</span></a></li>
+                    <li><a href="https://twitter.com/alexandra__to" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+                    <li><a href="mailto:a.to[at]northeastern.edu" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
+                    <li><a href="https://scholar.google.com/citations?user=FB_pYfIAAAAJ&hl=en" className="icon solid fa-graduation-cap"><span className="label">Google Scholar</span></a></li>
                 </ul>
             </div>
         </div>
